2d68bd7d1d3986fc3d7413554382a94c53d0290c
Zakariya Al <zzakaria14@gmail.com>
2021-03-06 12:45:10 +0100

    some fixes and changes

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import './collections-overview.styles.scss';
-import {selectCollections} from '../../redux/shop/shop.selectors';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-const CollectionsOverview = ({collections}) =>(
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-    <div className= 'collection-overview'>
-        {
-            collections.map(({id, ...otherCollectionProps}) =>(
-                <CollectionPreview key= {id} {...otherCollectionProps}/>
-                    ))
-        }
+import './collections-overview.styles.scss';
 
-    </div>
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,35 +1,31 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
 
-
-export const addItemToCart = (cartItems, cartItemToAdd) =>{
-
-    const existingCartItems = cartItems.find(
-        cartItem =>cartItem.id === cartItemToAdd.id
-
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
-    if(existingCartItems){
-        return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd.id ?
-            {...cartItem, quantity: cartItem.quantity + 1}
-            :cartItem
-            
-            )
-    }
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
 
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
 
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
 
-export const removeItemFromCart = (CartItems, CartItemToRemove) =>{
-    const existingCartItem = CartItems.find(
-        CartItem => CartItem.id === CartItemToRemove.id
-    )
-
-    if(existingCartItem.quantity === 1){
-        return CartItems.filter(CartItem => CartItem.id !== CartItemToRemove.id)
-
-    }
-    return CartItems.map(cartItem =>
-        cartItem.id === CartItemToRemove.id ?
-        {...cartItem, quantity: cartItem.quantity - 1}
-        :cartItem);
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
